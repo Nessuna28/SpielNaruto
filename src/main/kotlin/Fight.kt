96e425c6d12e5b1be66182c37383cf217edaec5a
Angelique Freier <freier-uede@web.de>
2023-05-03 21:50:17 +0200

    mit der Funktion für die Attackenauswahl begonnen

--- src/main/kotlin/Fight.kt
+++ src/main/kotlin/Fight.kt
@@ -1,18 +1,55 @@
-class Fight {
-
-    val selectionUser = ""
-    val selectionComputer = ""
-
-    fun stringToCharacter(){
-
-        if (characterUser in characterList){
+fun fight() {
+}
 
 
+fun selectionAttackUser() {
+
+    if (characterUser.name.isNotEmpty()) {
+        print("\nMöchtest du (1)angreifen oder (2)ausweichen? Gib die Zahl ein: ")
+        inputUser = readln()
+        if (inputUser.toInt() == 1) {
+            if (characterUser is CharacterWithGenjutsu) {
+                println(
+                    """
+                        
+            Mit welcher Attacke möchtest du angreifen?
+            Taijutsu
+            ${characterUser.ninjutsu.keys}
+            Genjutsu
+            ${characterUser.weapon}
+           
+            """.trimIndent()
+                )
+            } else if (characterUser is CharacterWithMedicalSkills) {
+                println(
+                    """
+                        
+            Mit welcher Attacke möchtest du angreifen?
+            Taijutsu
+            ${characterUser.ninjutsu.keys}
+            ${characterUser.weapon}
+            
+            """.trimIndent()
+                )
+                print("Oder möchtest du dich heilen? Gib Ja oder Nein ein: ")
+                inputUser = readln().lowercase()
+                if (inputUser == "ja") {
+                    (characterUser as CharacterWithMedicalSkills).heal()
+                }
+            } else {
+                println(
+                    """
+            Mit welcher Attacke möchtest du angreifen?
+            Taijutsu
+            ${characterUser.ninjutsu.keys}
+            ${characterUser.weapon}
+            
+            """.trimIndent()
+                )
+            }
+        } else if (inputUser.toInt() == 2) {
+            characterUser.baumstamm()
         }
     }
-
-    fun selectionAttack(){
-
-        println("\nMit welcher Attacke möchtest du angreifen? \n")
-    }
 }
+
